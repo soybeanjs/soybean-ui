0ff95f5e4d08ff8d4f47c2bea71e0d601fdf4a64
Soybean <soybeanjs@outlook.com>
2024-08-31 02:22:59 +0800

    optimize(packages): ui: optimize checkbox & radio style

--- packages/ui-variants/src/variants/checkbox.ts
+++ packages/ui-variants/src/variants/checkbox.ts
@@ -6,7 +6,7 @@ export const checkboxVariants = tv({
   slots: {
     root: 'flex items-center gap-2',
     control: [
-      'peer size-4.5 shrink-0 rounded-sm border shadow',
+      'peer size-4 shrink-0 rounded-sm border shadow',
       'focus-visible:(outline outline-2 outline-offset-2) disabled:(cursor-not-allowed opacity-50)',
       'data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
       'data-[state=indeterminate]:bg-primary data-[state=indeterminate]:text-primary-foreground'
@@ -17,25 +17,25 @@ export const checkboxVariants = tv({
   variants: {
     color: {
       primary: {
-        control: `border-primary data-[state=checked]:(bg-primary text-primary-foreground) data-[state=indeterminate]:(bg-primary text-primary-foreground)`
+        control: `border-primary focus-visible:outline-primary data-[state=checked]:(bg-primary text-primary-foreground) data-[state=indeterminate]:(bg-primary text-primary-foreground)`
       },
       destructive: {
-        control: `border-destructive data-[state=checked]:(bg-destructive text-destructive-foreground) data-[state=indeterminate]:(bg-destructive text-destructive-foreground)`
+        control: `border-destructive focus-visible:outline-destructive data-[state=checked]:(bg-destructive text-destructive-foreground) data-[state=indeterminate]:(bg-destructive text-destructive-foreground)`
       },
       success: {
-        control: `border-success data-[state=checked]:(bg-success text-success-foreground) data-[state=indeterminate]:(bg-success text-success-foreground)`
+        control: `border-success focus-visible:outline-success data-[state=checked]:(bg-success text-success-foreground) data-[state=indeterminate]:(bg-success text-success-foreground)`
       },
       warning: {
-        control: `border-warning data-[state=checked]:(bg-warning text-warning-foreground) data-[state=indeterminate]:(bg-warning text-warning-foreground)`
+        control: `border-warning focus-visible:outline-warning data-[state=checked]:(bg-warning text-warning-foreground) data-[state=indeterminate]:(bg-warning text-warning-foreground)`
       },
       info: {
-        control: `border-info data-[state=checked]:(bg-info text-info-foreground) data-[state=indeterminate]:(bg-info text-info-foreground)`
+        control: `border-info focus-visible:outline-info data-[state=checked]:(bg-info text-info-foreground) data-[state=indeterminate]:(bg-info text-info-foreground)`
       },
       secondary: {
-        control: `border-secondary-foreground/20 data-[state=checked]:(bg-secondary-foreground/20 text-secondary-foreground) data-[state=indeterminate]:(bg-secondary-foreground/20 text-secondary-foreground)`
+        control: `border-secondary-foreground focus-visible:outline-secondary-foreground/20 data-[state=checked]:(bg-secondary text-secondary-foreground) data-[state=indeterminate]:(bg-secondary text-secondary-foreground)`
       },
       accent: {
-        control: `border-accent-foreground/20 data-[state=checked]:(bg-accent-foreground/20 text-accent-foreground) data-[state=indeterminate]:(bg-accent-foreground/20 text-accent-foreground)`
+        control: `border-accent-foreground focus-visible:outline-accent-foreground/20 data-[state=checked]:(bg-accent text-accent-foreground) data-[state=indeterminate]:(bg-accent text-accent-foreground)`
       }
     },
     orientation: {
--- packages/ui-variants/src/variants/radio.ts
+++ packages/ui-variants/src/variants/radio.ts
@@ -9,38 +9,38 @@ export const radioVariants = tv({
       'peer flex justify-center items-center size-4 shrink-0 rounded-full border shadow',
       'focus-visible:(outline outline-2 outline-offset-2) disabled:(cursor-not-allowed opacity-50)'
     ],
-    indicator: 'size-2.5 rounded-full',
+    indicator: 'size-2 rounded-full',
     group: 'flex gap-2'
   },
   variants: {
     color: {
       primary: {
-        control: `border-primary`,
+        control: `border-primary focus-visible:outline-primary`,
         indicator: `bg-primary`
       },
       destructive: {
-        control: `border-destructive`,
+        control: `border-destructive focus-visible:outline-destructive`,
         indicator: `bg-destructive`
       },
       success: {
-        control: `border-success`,
+        control: `border-success focus-visible:outline-success`,
         indicator: `bg-success`
       },
       warning: {
-        control: `border-warning`,
+        control: `border-warning focus-visible:outline-warning`,
         indicator: `bg-warning`
       },
       info: {
-        control: `border-info`,
+        control: `border-info focus-visible:outline-info`,
         indicator: `bg-info`
       },
       secondary: {
-        control: `border-secondary-foreground`,
-        indicator: `bg-secondary-foreground`
+        control: `border-secondary-foreground focus-visible:outline-secondary-foreground/20`,
+        indicator: `bg-secondary-foreground/50`
       },
       accent: {
-        control: `border-accent-foreground`,
-        indicator: `bg-accent-foreground`
+        control: `border-accent-foreground focus-visible:outline-accent-foreground/20`,
+        indicator: `bg-accent-foreground/50`
       }
     },
     orientation: {
