b0638204a9e717611cf577439174c8e0c5d6875d
Azir <2075125282@qq.com>
2024-10-03 18:36:00 +0800

    feat(packages): add chip.

---
+++ packages/ui/src/components/chip/chip-root.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { Primitive } from 'radix-vue';
+import { chipVariants, cn } from '@soybean-ui/variants';
+import type { ChipRootProps } from './types';
+
+defineOptions({
+  name: 'SChipRoot'
+});
+
+const { class: cls } = defineProps<ChipRootProps>();
+
+const { root } = chipVariants();
+
+const mergedCls = computed(() => cn(root(), cls));
+</script>
+
+<template>
+  <Primitive as="div" :class="mergedCls">
+    <slot />
+  </Primitive>
+</template>
