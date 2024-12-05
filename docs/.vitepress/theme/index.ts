3ffe5d0252ad5682feb63465b70e51e640ddaf2a
Soybean <soybeanjs@outlook.com>
2024-12-05 17:40:18 +0800

    chore(projects): update package name

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress';
 import 'uno.css';
 import '@unocss/reset/tailwind.css';
+import '@soybean-ui/vue/style.css';
 import './style.css';
 import Layout from './layout/index.vue';
 
