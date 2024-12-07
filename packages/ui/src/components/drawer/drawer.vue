7a2edf873c9f146417d0547aa8de3e30e7f9cc8a
Soybean <soybeanjs@outlook.com>
2024-12-07 13:14:21 +0800

    fix(packages): fix generate style repeatly

--- packages/ui/src/components/drawer/drawer.vue
+++ packages/ui/src/components/drawer/drawer.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
+import { useStyleTag } from '@vueuse/core';
 import {
   DrawerPortal,
   DrawerRoot,
   DrawerTrigger,
-  Primitive,
-  TeleportPrimitive,
   useForwardPropsEmits,
   useOmitForwardProps,
   usePickForwardProps
@@ -65,6 +64,112 @@ const forwardedRoot = useForwardPropsEmits(forwardedRootProps, emit);
 const forwardedContentProps = usePickForwardProps(props, [...cardPropKeys, 'showClose', 'closeClass', 'cardClass']);
 
 const cardSlotKeys = Object.keys(slots).filter(slot => slot !== 'trigger') as (keyof Slots)[];
+
+const css = `
+[soybean-drawer] {
+  touch-action: none;
+  will-change: transform;
+  transition: transform 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+}
+[soybean-drawer][soybean-drawer-direction='bottom'] {
+  transform: translate3d(0, 100%, 0);
+}
+[soybean-drawer][soybean-drawer-direction='top'] {
+  transform: translate3d(0, -100%, 0);
+}
+[soybean-drawer][soybean-drawer-direction='left'] {
+  transform: translate3d(-100%, 0, 0);
+}
+[soybean-drawer][soybean-drawer-direction='right'] {
+  transform: translate3d(100%, 0, 0);
+}
+.soybean-drawer-dragging [vault-drawer-direction='top'] {
+  overflow-y: hidden !important;
+}
+.soybean-drawer-dragging [vault-drawer-direction='bottom'] {
+  overflow-y: hidden !important;
+}
+.soybean-drawer-dragging [vault-drawer-direction='left'] {
+  overflow-x: hidden !important;
+}
+.soybean-drawer-dragging [vault-drawer-direction='right'] {
+  overflow-x: hidden !important;
+}
+[soybean-drawer][soybean-drawer-visible='true'][soybean-drawer-direction='top'] {
+  transform: translate3d(0, var(--snap-point-height, 0), 0);
+}
+[soybean-drawer][soybean-drawer-visible='true'][soybean-drawer-direction='bottom'] {
+  transform: translate3d(0, var(--snap-point-height, 0), 0);
+}
+[soybean-drawer][soybean-drawer-visible='true'][soybean-drawer-direction='left'] {
+  transform: translate3d(var(--snap-point-height, 0), 0, 0);
+}
+[soybean-drawer][soybean-drawer-visible='true'][soybean-drawer-direction='right'] {
+  transform: translate3d(var(--snap-point-height, 0), 0, 0);
+}
+[soybean-drawer-overlay] {
+  opacity: 0;
+  transition: opacity 0.5s cubic-bezier(0.32, 0.72, 0, 1);
+}
+[soybean-drawer-overlay][soybean-drawer-visible='true'] {
+  opacity: 1;
+}
+[soybean-drawer]::after {
+  content: '';
+  position: absolute;
+  background: inherit;
+  background-color: inherit;
+}
+[soybean-drawer][soybean-drawer-direction='top']::after {
+  top: initial;
+  bottom: 100%;
+  left: 0;
+  right: 0;
+  height: 200%;
+}
+[soybean-drawer][soybean-drawer-direction='bottom']::after {
+  top: 100%;
+  bottom: initial;
+  left: 0;
+  right: 0;
+  height: 200%;
+}
+[soybean-drawer][soybean-drawer-direction='left']::after {
+  left: initial;
+  right: 100%;
+  top: 0;
+  bottom: 0;
+  width: 200%;
+}
+[soybean-drawer][soybean-drawer-direction='right']::after {
+  left: 100%;
+  right: initial;
+  top: 0;
+  bottom: 0;
+  width: 200%;
+}
+[soybean-drawer-overlay][soybean-drawer-snap-points='true']:not([soybean-drawer-snap-points-overlay='true']):not(
+    [data-state='closed']
+  ) {
+  opacity: 0;
+}
+[soybean-drawer-overlay][soybean-drawer-snap-points-overlay='true']:not([soybean-drawer-visible='false']) {
+  opacity: 1;
+} /* This will allow us to not animate via animation, but still benefit from delaying unmount via
+      Radix. */
+@keyframes fake-animation {
+  from {
+  }
+  to {
+  }
+}
+@media (hover: hover) and (pointer: fine) {
+  [soybean-drawer] {
+    user-select: none;
+  }
+}
+`;
+useStyleTag(css, { id: 'soybean-drawer-style' });
 </script>
 
 <template>
@@ -81,38 +186,4 @@ const cardSlotKeys = Object.keys(slots).filter(slot => slot !== 'trigger') as (k
       </SDrawerContent>
     </DrawerPortal>
   </DrawerRoot>
-  <TeleportPrimitive to="head">
-    <Primitive as="style">
-      [soybean-drawer] { touch-action: none; will-change: transform; transition: transform 0.5s cubic-bezier(0.32, 0.72,
-      0, 1); } [soybean-drawer][soybean-drawer-direction='bottom'] { transform: translate3d(0, 100%, 0); }
-      [soybean-drawer][soybean-drawer-direction='top'] { transform: translate3d(0, -100%, 0); }
-      [soybean-drawer][soybean-drawer-direction='left'] { transform: translate3d(-100%, 0, 0); }
-      [soybean-drawer][soybean-drawer-direction='right'] { transform: translate3d(100%, 0, 0); }
-      .soybean-drawer-dragging [vault-drawer-direction='top'] { overflow-y: hidden !important; }
-      .soybean-drawer-dragging [vault-drawer-direction='bottom'] { overflow-y: hidden !important; }
-      .soybean-drawer-dragging [vault-drawer-direction='left'] { overflow-x: hidden !important; }
-      .soybean-drawer-dragging [vault-drawer-direction='right'] { overflow-x: hidden !important; }
-      [soybean-drawer][soybean-drawer-visible='true'][soybean-drawer-direction='top'] { transform: translate3d(0,
-      var(--snap-point-height, 0), 0); }
-      [soybean-drawer][soybean-drawer-visible='true'][soybean-drawer-direction='bottom'] { transform: translate3d(0,
-      var(--snap-point-height, 0), 0); }
-      [soybean-drawer][soybean-drawer-visible='true'][soybean-drawer-direction='left'] { transform:
-      translate3d(var(--snap-point-height, 0), 0, 0); }
-      [soybean-drawer][soybean-drawer-visible='true'][soybean-drawer-direction='right'] { transform:
-      translate3d(var(--snap-point-height, 0), 0, 0); } [soybean-drawer-overlay] { opacity: 0; transition: opacity 0.5s
-      cubic-bezier(0.32, 0.72, 0, 1); } [soybean-drawer-overlay][soybean-drawer-visible='true'] { opacity: 1; }
-      [soybean-drawer]::after { content: ''; position: absolute; background: inherit; background-color: inherit; }
-      [soybean-drawer][soybean-drawer-direction='top']::after { top: initial; bottom: 100%; left: 0; right: 0; height:
-      200%; } [soybean-drawer][soybean-drawer-direction='bottom']::after { top: 100%; bottom: initial; left: 0; right:
-      0; height: 200%; } [soybean-drawer][soybean-drawer-direction='left']::after { left: initial; right: 100%; top: 0;
-      bottom: 0; width: 200%; } [soybean-drawer][soybean-drawer-direction='right']::after { left: 100%; right: initial;
-      top: 0; bottom: 0; width: 200%; }
-      [soybean-drawer-overlay][soybean-drawer-snap-points='true']:not([soybean-drawer-snap-points-overlay='true']):not(
-      [data-state='closed'] ) { opacity: 0; }
-      [soybean-drawer-overlay][soybean-drawer-snap-points-overlay='true']:not([soybean-drawer-visible='false']) {
-      opacity: 1; } /* This will allow us to not animate via animation, but still benefit from delaying unmount via
-      Radix. */ @keyframes fake-animation { from { } to { } } @media (hover: hover) and (pointer: fine) {
-      [soybean-drawer] { user-select: none; } }
-    </Primitive>
-  </TeleportPrimitive>
 </template>
