5a5ab2148e907b06e6779a5ad83315ef0d715b83
Soybean <soybeanjs@outlook.com>
2024-12-19 12:51:52 +0800

    feat(packages): ui: add button-link (#51)

---
+++ packages/ui/src/components/button/button-link.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useForwardProps } from '@soybean-ui/primitive';
+import { SButton } from '../button';
+import type { ButtonLinkProps } from './types';
+
+defineOptions({
+  name: 'SButtonLink'
+});
+
+const props = withDefaults(defineProps<ButtonLinkProps>(), {
+  variant: 'link',
+  target: '_blank',
+  rel: 'noopener noreferrer'
+});
+
+const forwardedProps = useForwardProps(props);
+</script>
+
+<template>
+  <SButton v-bind="forwardedProps" as="a">
+    <slot />
+  </SButton>
+</template>
--- packages/ui/src/components/button/index.ts
+++ packages/ui/src/components/button/index.ts
@@ -1,8 +1,9 @@
 import SButton from './button.vue';
 import SButtonIcon from './button-icon.vue';
 import SLoadingButton from './loading-button.vue';
+import SButtonLink from './button-link.vue';
 import SButtonGroup from './button-group.vue';
 
-export { SButton, SButtonIcon, SLoadingButton, SButtonGroup };
+export { SButton, SButtonIcon, SLoadingButton, SButtonLink, SButtonGroup };
 
 export * from './types';
