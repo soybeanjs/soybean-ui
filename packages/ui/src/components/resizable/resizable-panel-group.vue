54a41b4078c27fae652394571fb91f53a9a52898
Soybean <soybeanjs@outlook.com>
2025-02-20 00:48:09 +0800

    feat(packages): ui: add resizable (#56)

---
+++ packages/ui/src/components/resizable/resizable-panel-group.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { SplitterGroup, useForwardPropsEmits } from '@soybean-ui/primitives';
+import { cn, resizableVariants } from '@soybean-ui/variants';
+import type { ResizablePanelGroupEmits, ResizablePanelGroupProps } from './types';
+
+defineOptions({
+  name: 'SResizablePanelGroup'
+});
+
+const { class: cls, ...delegatedProps } = defineProps<ResizablePanelGroupProps>();
+
+const emit = defineEmits<ResizablePanelGroupEmits>();
+
+const forwarded = useForwardPropsEmits(delegatedProps, emit);
+
+const { panelGroup } = resizableVariants();
+
+const mergedCls = computed(() => cn(panelGroup(), cls));
+</script>
+
+<template>
+  <SplitterGroup v-bind="forwarded" :class="mergedCls">
+    <slot />
+  </SplitterGroup>
+</template>
