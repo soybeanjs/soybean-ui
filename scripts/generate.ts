b76d142861875c3a5685df980ca6344a8d3c2578
Soybean <soybeanjs@outlook.com>
2025-02-23 02:06:59 +0800

    optimize(projects): optimize scripts generate

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,15 +1,28 @@
 import fs from 'node:fs';
 import fg from 'fast-glob';
 
-function getComponentFiles(dir: string, module: string) {
-  const files = fg.sync(`**/*.vue`, { onlyFiles: true, cwd: dir });
-  files.forEach(file => {
-    const componentName = file.replace('.vue', '');
-    generateComponent(toKebabCase(componentName), module, dir);
-  });
-}
+function getComponentFiles(module: string) {
+  const kbName = toKebabCase(module);
+  const dir = `packages/ui/src/components/${kbName}`;
 
-getComponentFiles('packages/ui/src/components/sidebar', 'sidebar');
+  const files = fg.sync(`**/*.vue`, { onlyFiles: true, cwd: dir });
+
+  const componentNames: string[] = [];
+
+  files
+    .filter(file => file.includes('-'))
+    .forEach(file => {
+      const componentName = file.replace('.vue', '');
+
+      componentNames.push(toKebabCase(componentName));
+
+      generateComponent(toKebabCase(componentName), module, dir);
+    });
+
+  generateTypes(componentNames, dir);
+  generateExports(componentNames, dir);
+  generateVariants(componentNames, module);
+}
 
 function toKebabCase(str: string) {
   return str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
@@ -59,3 +72,68 @@ function generateComponent(componentName: string, module: string, dir: string) {
 
   fs.writeFileSync(filePath, template);
 }
+
+function generateTypes(componentNames: string[], dir: string) {
+  const filePath = `${dir}/types.ts`;
+  let template = `import type {\n`;
+
+  componentNames.forEach(componentName => {
+    template += `  ${toPascalCase(componentName)}Props as _${toPascalCase(componentName)}Props,\n`;
+  });
+
+  template += `} from '@soybean-ui/primitives';\n\n`;
+
+  componentNames.forEach(componentName => {
+    template += `export interface ${toPascalCase(componentName)}Props extends _${toPascalCase(componentName)}Props {}\n\n`;
+  });
+
+  fs.writeFileSync(filePath, template);
+}
+
+function generateExports(componentNames: string[], dir: string) {
+  const filePath = `${dir}/index.ts`;
+  let template = ``;
+
+  componentNames.forEach(componentName => {
+    template += `import S${toPascalCase(componentName)} from './${componentName}.vue';\n`;
+  });
+
+  template += `\nexport {\n`;
+
+  componentNames.forEach(componentName => {
+    template += `  S${toPascalCase(componentName)},\n`;
+  });
+
+  template += `}\nexport * from './types';`;
+
+  fs.writeFileSync(filePath, template);
+}
+
+function generateVariants(componentNames: string[], module: string) {
+  const kbName = toKebabCase(module);
+  const variantsDir = `packages/variants/src/variants/${kbName}.ts`;
+  let template = `// @unocss-include
+import { tv } from 'tailwind-variants';
+
+export const ${module}Variants = tv({
+  slots: {\n`;
+
+  componentNames.forEach(componentName => {
+    const slotName = toCamelCase(toCamelCase(componentName).replace(module, '')) || 'root';
+
+    template += `    ${slotName}: '',\n`;
+  });
+
+  template += `  }\n`;
+  template += `});\n\n`;
+
+  template += `export type ${toPascalCase(module)}Slots = keyof typeof ${module}Variants.slots;`;
+
+  fs.writeFileSync(variantsDir, template);
+}
+
+function start() {
+  getComponentFiles('numberField');
+}
+
+start();
