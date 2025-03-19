9d81a394807da3fdf11869747454c073100f3f95
Soybean <soybeanjs@outlook.com>
2025-03-19 16:27:14 +0800

    fix(styles): fix docs style

--- docs/.vitepress/demo/chip.vue
+++ docs/.vitepress/demo/chip.vue
@@ -17,10 +17,9 @@ const show = ref(true);
 </script>
 
 <template>
-  <div class="w-full flex justify-center"></div>
-  <div class="w-320px lt-sm:w-auto">
+  <div>
     <div class="py-12px text-18px">Chip Size</div>
-    <div class="flex gap-3">
+    <div class="flex flex-wrap gap-3">
       <div v-for="size in sizes" :key="size">
         <div>{{ size }}</div>
         <SChip :size="size">
@@ -29,7 +28,7 @@ const show = ref(true);
       </div>
     </div>
     <div class="py-12px text-18px">Chip Color</div>
-    <div class="flex gap-3">
+    <div class="flex flex-wrap gap-3">
       <div v-for="color in colors" :key="color">
         <SChip :color="color">
           <SButton variant="dashed">{{ color }}</SButton>
@@ -37,7 +36,7 @@ const show = ref(true);
       </div>
     </div>
     <div class="py-12px text-18px">Chip Color With Text</div>
-    <div class="flex gap-4">
+    <div class="flex flex-wrap gap-4">
       <div v-for="color in colors" :key="color">
         <SChip :color="color" text="99+">
           <SButton variant="dashed">{{ color }}</SButton>
@@ -45,7 +44,7 @@ const show = ref(true);
       </div>
     </div>
     <div class="py-12px text-18px">Chip Positions</div>
-    <div class="flex gap-3">
+    <div class="flex flex-wrap gap-3">
       <div v-for="position in positions" :key="position">
         <SChip :position="position">
           <SButton variant="dashed" class="w-30">{{ position }}</SButton>
@@ -53,14 +52,14 @@ const show = ref(true);
       </div>
     </div>
     <div class="py-12px text-18px">Chip modelValue</div>
-    <div class="flex gap-3">
+    <div class="flex flex-wrap gap-3">
       <SInput v-model="text"></SInput>
       <SChip :text="text">
         <SButton variant="outline">A</SButton>
       </SChip>
     </div>
     <div class="py-12px text-18px">Chip Slot</div>
-    <div class="flex gap-3">
+    <div class="flex flex-wrap gap-3">
       <SInput v-model="slotText"></SInput>
       <SChip :text="slotText">
         <SButton variant="outline">A</SButton>
@@ -68,7 +67,7 @@ const show = ref(true);
       </SChip>
     </div>
     <div class="py-12px text-18px">Chip Show</div>
-    <div class="flex gap-3">
+    <div class="flex flex-wrap gap-3">
       <SSwitch v-model:checked="show"></SSwitch>
       <SChip :show="show">
         <SButton variant="outline">A</SButton>
