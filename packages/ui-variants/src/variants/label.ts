7214a8f9c5fe0936b65087781727371f8940d2f1
Soybean <soybeanjs@outlook.com>
2025-03-29 23:59:44 +0800

    refactor(packages): ui: optimize checkbox and adapt to `ThemeSize`

--- packages/ui-variants/src/variants/label.ts
+++ packages/ui-variants/src/variants/label.ts
@@ -5,12 +5,12 @@ export const labelVariants = tv({
   base: 'font-medium leading-none peer-disabled:(cursor-not-allowed opacity-70)',
   variants: {
     size: {
-      xs: 'text-xs',
-      sm: 'text-sm',
+      xs: 'text-2xs',
+      sm: 'text-xs',
       md: 'text-sm',
       lg: 'text-base',
-      xl: 'text-base',
-      '2xl': 'text-lg'
+      xl: 'text-lg',
+      '2xl': 'text-xl'
     }
   },
   defaultVariants: {
