089c214db18941c6c55149c2a827e8170a27e317
Azir-11 <2075125282@qq.com>
2025-04-09 00:02:08 +0800

    docs(projects):  update component preview.

--- docs/.vitepress/theme/components/component-preview.vue
+++ docs/.vitepress/theme/components/component-preview.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, getCurrentInstance, ref } from 'vue';
 import { useClipboard } from '@vueuse/core';
-import { SButtonIcon, SSegment } from 'soy-ui';
+import { SButtonIcon, SCard, SSegment } from 'soy-ui';
 import type { SegmentOptionData } from 'soy-ui';
 import { kebabCase } from 'es-toolkit';
 import { Copy, CopyCheck } from 'lucide-vue-next';
@@ -14,10 +14,12 @@ defineOptions({
 interface Props {
   name: string;
   demo?: string;
+  title?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  demo: undefined
+  demo: undefined,
+  title: undefined
 });
 
 const instance = getCurrentInstance();
@@ -57,22 +59,32 @@ const active = ref('preview');
 const code = computed(() => getCode(props.name, props.demo));
 
 const codeHtml = computed(() => highlight(code.value, 'vue'));
+
+const cardUi = {
+  title: 'text-xl'
+};
 </script>
 
 <template>
-  <div class="flex justify-end py-4">
-    <SSegment v-model="active" :items="items" />
-  </div>
-
-  <slot v-if="active === 'preview'"></slot>
-  <div v-else class="relative">
-    <SButtonIcon class="absolute right-5 top-3 text-muted-foreground" @click="copy(code)">
-      <Copy v-if="!copied" />
-      <CopyCheck v-else />
-    </SButtonIcon>
-    <!-- eslint-disable-next-line vue/no-v-html -->
-    <div class="h-[calc(100vh-380px)] overflow-auto" v-html="codeHtml" />
-  </div>
+  <SCard :title="title || demo || name" :ui="cardUi" class="mb-12px">
+    <template #extra>
+      <div class="flex justify-end">
+        <SSegment v-model="active" :items="items" />
+      </div>
+    </template>
+    <slot v-if="active === 'preview'"></slot>
+    <div v-else class="relative">
+      <SButtonIcon
+        class="absolute right-5 top-3 text-muted-foreground"
+        @click="copy(code)"
+      >
+        <Copy v-if="!copied" />
+        <CopyCheck v-else />
+      </SButtonIcon>
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <div class="h-[calc(100vh-380px)] overflow-auto" v-html="codeHtml" />
+    </div>
+  </SCard>
 </template>
 
 <style scoped>
--- docs/docs/components/accordion.md
+++ docs/docs/components/accordion.md
@@ -1,21 +1,11 @@
 # Accordion
 
-## Single
-
 <ComponentPreview name="Accordion" demo="Single" />
-
-## Multiple
 
 <ComponentPreview name="Accordion" demo="Multiple" />
 
-## Custom Styling
-
 <ComponentPreview name="Accordion" demo="CustomStyling" />
 
-## Custom Icon
-
 <ComponentPreview name="Accordion" demo="CustomIcon" />
-
-## Size
 
 <ComponentPreview name="Accordion" demo="Size" />
