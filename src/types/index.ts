dd3d0c5c2f7ab9e58183111b9cb56a9daab65341
Soybean <soybeanjs@outlook.com>
2025-05-21 21:22:56 +0800

    feat(types): define new types for class values, props context, and open state management

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+import type { ComputedRef } from 'vue';
+
+export type ClassValue = string | null | undefined | Record<string, boolean> | ClassValue[];
+export type ClassValueProp = {
+  /** class name */
+  class?: ClassValue;
+};
+
+export type PropsToContext<T, K extends keyof T> = {
+  [P in K]-?: ComputedRef<T[P]>;
+};
+
+export type OpenState = 'open' | 'closed';
