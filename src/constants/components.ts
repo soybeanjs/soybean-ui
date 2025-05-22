30398cd03e6d940392dac9d96573639b2ae54d9a
Soybean <soybeanjs@outlook.com>
2025-05-23 01:03:54 +0800

    feat(constants): add new component entries for accordion, collapsible, configProvider, and portal

--- src/constants/components.ts
+++ src/constants/components.ts
@@ -1,4 +1,8 @@
 export const components = {
+  accordion: ['AccordionRoot', 'AccordionItem', 'AccordionTrigger', 'AccordionHeader', 'AccordionContent'],
+  collapsible: ['CollapsibleRoot', 'CollapsibleTrigger', 'CollapsibleContent'],
+  configProvider: ['ConfigProvider'],
+  portal: ['Portal'],
   primitive: ['Primitive'],
   slot: ['Slot']
 };
