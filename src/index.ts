8c19d5a82882064943355e31409864ba131b09ea
Soybean <soybeanjs@outlook.com>
2025-05-23 01:22:42 +0800

    refactor(index): reorganize exports to include accordion, config-provider, and maintain existing components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-export * from './components/slot';
-export * from './components/primitive';
-export * from './components/portal';
+export * from './components/accordion';
 export * from './components/collapsible';
+export * from './components/config-provider';
+export * from './components/portal';
+export * from './components/primitive';
+export * from './components/slot';
 
 export type { ClassValue, ClassValueProp, OpenState } from './types';
