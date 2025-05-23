e08d4d4dd80adad4588d046578babcb819518008
Soybean <soybeanjs@outlook.com>
2025-05-24 04:20:39 +0800

    feat(dialog): add dialog example component showcasing usage of DialogRoot, DialogTrigger, and related elements

---
+++ playground/src/examples/dialog.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { buttonVariants, dialogVariants } from '@soybean-ui/variants';
+import { X } from 'lucide-vue-next';
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogOverlay,
+  DialogPortal,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger
+} from '../../../src/components/dialog';
+import SectionWrapper from '../components/section-wrapper.vue';
+
+const { closeIcon, content, description, overlay, title } = dialogVariants();
+</script>
+
+<template>
+  <SectionWrapper title="Dialog">
+    <DialogRoot>
+      <DialogTrigger as="template">
+        <button :class="buttonVariants({ variant: 'pure' })" class="w-20">Open</button>
+      </DialogTrigger>
+      <DialogPortal>
+        <DialogOverlay :class="overlay()" />
+        <DialogContent :class="content()">
+          <DialogTitle :class="title()">Dialog Title</DialogTitle>
+          <DialogDescription :class="description()">Dialog Description</DialogDescription>
+          <DialogClose as="template" :class="closeIcon()">
+            <button>
+              <X />
+            </button>
+          </DialogClose>
+          <p>This is a dialog</p>
+        </DialogContent>
+      </DialogPortal>
+    </DialogRoot>
+  </SectionWrapper>
+</template>
