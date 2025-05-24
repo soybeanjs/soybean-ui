1cba28d1309ad3ace33cf0dce0cf572c49236ee0
Soybean <soybeanjs@outlook.com>
2025-05-24 21:57:31 +0800

    feat(tests): enhance testing setup with additional commands and configuration for coverage and UI

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ import vue from 'unplugin-vue/vite';
 export default defineConfig({
   plugins: [vue()],
   test: {
+    globals: true,
+    include: ['test/specs/**/*.spec.ts'],
     environment: 'happy-dom'
   }
 });
