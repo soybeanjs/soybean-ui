329ac3469be664bf52f3211b430c6d0a88cef3fc
Soybean <soybeanjs@outlook.com>
2025-05-25 02:14:27 +0800

    feat(vite): add vite-plugin-vue-devtools to enhance Vue development experience

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
+import devtools from 'vite-plugin-vue-devtools';
 import vue from 'unplugin-vue/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import unocss from 'unocss/vite';
 
 export default defineConfig({
   root: './playground',
-  plugins: [vue(), tsconfigPaths(), unocss()],
+  plugins: [vue(), tsconfigPaths(), unocss(), devtools()],
   server: {
     open: true
   }
