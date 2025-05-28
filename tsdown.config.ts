00fb806a459a2042d2d602939f9ec8bbb082d6cf
Soybean <soybeanjs@outlook.com>
2025-05-28 10:18:37 +0800

    refactor(tsdown.config): update entry points to dynamically include component indices using fast-glob

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'tsdown';
 import unpluginVue from 'unplugin-vue/rolldown';
 import unpluginVueJsx from 'unplugin-vue-jsx/rolldown';
+import fg from 'fast-glob';
 import pkg from './package.json';
 
 export default defineConfig({
   entry: [
-    'src/index.ts',
-    'src/constant/index.ts',
-    'src/composables/index.ts',
-    'src/nuxt/index.ts',
-    'src/resolver/index.ts'
+    ...fg.sync('src/components/**/index.ts'),
+    ...fg.sync('src/*/index.ts').filter(glob => glob !== 'src/types/index.ts'),
+    'src/index.ts'
   ],
   platform: 'neutral',
   external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.devDependencies), '@vue/shared'],
