8a25f601147e6371a15f4081d44f3dbba64083fd
Soybean <soybeanjs@outlook.com>
2025-05-28 10:10:13 +0800

    chore(projects): use tsdown unbundle mode

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,55 +1,25 @@
 import { defineConfig } from 'tsdown';
 import unpluginVue from 'unplugin-vue/rolldown';
 import unpluginVueJsx from 'unplugin-vue-jsx/rolldown';
-import fg from 'fast-glob';
+import pkg from './package.json';
 
-export default defineConfig(() => {
-  const entry = getEntry();
-
-  return {
-    entry,
-    platform: 'neutral',
-    external: [
-      'vue',
-      '@vue/shared',
-      '@unocss/reset',
-      '@nuxt/schema',
-      '@nuxt/kit',
-      'unplugin-vue-components',
-      'lucide-vue-next',
-      '@soybean-ui/variants'
-    ],
-    clean: true,
-    dts: {
-      vue: true
-    },
-    plugins: [unpluginVue({ isProduction: true }), unpluginVueJsx()],
-    sourcemap: false,
-    minify: false
-  };
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+    'src/constant/index.ts',
+    'src/composables/index.ts',
+    'src/nuxt/index.ts',
+    'src/resolver/index.ts'
+  ],
+  platform: 'neutral',
+  external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.devDependencies), '@vue/shared'],
+  noExternal: ['aria-hidden'],
+  clean: true,
+  dts: {
+    vue: true
+  },
+  unbundle: true,
+  plugins: [unpluginVue({ isProduction: true }), unpluginVueJsx()],
+  sourcemap: false,
+  minify: false
 });
-
-function getEntry() {
-  const entry: Record<string, string> = {};
-
-  const names = fg.sync('src/*/index.ts');
-  const excludes = ['types'];
-  names.forEach(n => {
-    const name = n.replace('/index.ts', '').replace('src/', '');
-    if (excludes.includes(name)) return;
-    entry[`${name}/index`] = n;
-  });
-
-  // components entry
-  const components = fg.sync('src/components/**/index.ts');
-  components.forEach(component => {
-    const name = component.replace('/index.ts', '').replace('src/components/', '');
-    entry[`components/${name}/index`] = component;
-  });
-
-  // index entry
-  entry.index = 'src/index.ts';
-  entry.types = 'src/types/index.ts';
-
-  return entry;
-}
