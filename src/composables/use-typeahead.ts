bdf4af7f194265a2fe724f1cf2c5dfe7307a3d31
Soybean <soybeanjs@outlook.com>
2025-06-02 05:55:00 +0800

    feat(composables): add useTypeahead composable for enhanced search functionality

---
+++ src/composables/use-typeahead.ts
@@ -0,0 +1,43 @@
+import { getActiveElement, getNextMatch, refAutoReset } from '../shared';
+import type { CollectionItemData } from './use-collection';
+
+export function useTypeahead(callback?: (search: string) => void) {
+  // Reset `search` 1 second after it was last updated
+  const search = refAutoReset('', 1000);
+
+  const handleTypeaheadSearch = (key: string, items: CollectionItemData[]) => {
+    search.value += key;
+
+    if (callback) {
+      callback(key);
+      return undefined;
+    }
+
+    const currentItem = getActiveElement();
+    const itemsWithTextValue = items.map(item => ({
+      ...item,
+      textValue: item.data?.textValue ?? item.element.textContent?.trim() ?? ''
+    }));
+    const currentMatch = itemsWithTextValue.find(item => item.element === currentItem);
+    const values = itemsWithTextValue.map(item => item.textValue);
+    const nextMatch = getNextMatch(values, search.value, currentMatch?.textValue);
+
+    const newItem = itemsWithTextValue.find(item => item.textValue === nextMatch);
+
+    if (newItem) {
+      newItem.element.focus();
+    }
+
+    return newItem?.element;
+  };
+
+  const resetTypeahead = () => {
+    search.value = '';
+  };
+
+  return {
+    search,
+    handleTypeaheadSearch,
+    resetTypeahead
+  };
+}
