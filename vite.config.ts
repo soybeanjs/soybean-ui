881b2b26b70441f13da74264fe754508d4ec0046
Soybean <soybeanjs@outlook.com>
2025-06-05 07:58:22 +0800

    chore(config): update Vite configuration to include path aliasing for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
+import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import devtools from 'vite-plugin-vue-devtools';
 import vue from 'unplugin-vue/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import unocss from 'unocss/vite';
 
 export default defineConfig({
   root: './playground',
-  plugins: [vue(), tsconfigPaths({ root: './', ignoreConfigErrors: true }), unocss(), devtools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  plugins: [vue(), unocss(), devtools()],
   server: {
     open: true
   }
