5925655b7d607cbdb2e44ccc884cefd7c11587bf
Soybean <soybeanjs@outlook.com>
2025-06-06 03:31:42 +0800

    fix(collapsible-content): enhance animation handling and improve style management during mount

--- src/components/collapsible/collapsible-content.vue
+++ src/components/collapsible/collapsible-content.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { computed, nextTick, onMounted, shallowRef } from 'vue';
-import type { CSSProperties } from 'vue';
+import { computed, nextTick, onMounted, shallowRef, watch } from 'vue';
 import { useForwardElement, usePresence } from '../../composables';
 import { Primitive } from '../primitive';
 import { useCollapsibleRootContext } from './context';
@@ -18,20 +17,13 @@ const [contentElement, setContentElement] = useForwardElement();
 const { contentId, initContentId, open, dataDisabled, dataState, unmountOnHide } =
   useCollapsibleRootContext('CollapsibleContent');
 
-let originalStyles: Pick<CSSStyleDeclaration, 'transitionDuration' | 'animationName'>;
-
-initContentId();
-
 const isPresent = props.forceMount ? shallowRef(true) : usePresence(contentElement, open, handleNodeStyle);
 
 // when opening we want it to immediately open to retrieve dimensions
 // when closing we delay `present` to retrieve dimensions before closing
 const isOpen = computed(() => isPresent.value || open.value);
-const $isOpen = isOpen.value;
-
-const lockAnimationStyles = shallowRef<CSSProperties | undefined>(
-  $isOpen ? { transitionDuration: '0s !important', animationName: 'none !important' } : undefined
-);
+let isMountAnimationPrevented = open.value;
+let originalStyles: Pick<CSSStyleDeclaration, 'transitionDuration' | 'animationName'>;
 
 const hidden = computed(() => {
   if (isOpen.value) {
@@ -40,12 +32,6 @@ const hidden = computed(() => {
 
   return unmountOnHide.value ? '' : 'until-found';
 });
-
-const style = computed<CSSProperties>(() => ({
-  [collapsibleContentCssVars.width]: '0px',
-  [collapsibleContentCssVars.height]: '0px',
-  ...lockAnimationStyles.value
-}));
 
 function handleNodeStyle() {
   const node = contentElement.value;
@@ -68,16 +54,28 @@ function handleNodeStyle() {
   nodeStyle.setProperty(collapsibleContentCssVars.width, `${rect.width}px`);
   nodeStyle.setProperty(collapsibleContentCssVars.height, `${rect.height}px`);
 
-  // kick off any animations/transitions that were originally set up if it isn't the initial mount
-  nodeStyle.transitionDuration = originalStyles.transitionDuration;
-  nodeStyle.animationName = originalStyles.animationName;
+  if (!isMountAnimationPrevented) {
+    // kick off any animations/transitions that were originally set up if it isn't the initial mount
+    nodeStyle.transitionDuration = originalStyles.transitionDuration;
+    nodeStyle.animationName = originalStyles.animationName;
+  }
 }
 
-onMounted(async () => {
-  if (!$isOpen) return;
-  lockAnimationStyles.value = undefined;
-  await nextTick();
-  handleNodeStyle();
+watch(
+  isOpen,
+  async () => {
+    await nextTick();
+    handleNodeStyle();
+  },
+  { immediate: true, flush: 'post' }
+);
+
+initContentId();
+
+onMounted(() => {
+  requestAnimationFrame(() => {
+    isMountAnimationPrevented = false;
+  });
 });
 </script>
 
@@ -89,7 +87,10 @@ onMounted(async () => {
     :data-disabled="dataDisabled"
     :data-state="dataState"
     :hidden="hidden"
-    :style="style"
+    :style="{
+      [collapsibleContentCssVars.width]: '0px',
+      [collapsibleContentCssVars.height]: '0px'
+    }"
   >
     <slot v-if="unmountOnHide ? isOpen : true" />
   </Primitive>
