d6b61edef0f1db26bec37f3b4bbb5d4efea75742
Soybean <soybeanjs@outlook.com>
2025-06-08 16:37:05 +0800

    refactor(use-props): enhance usePickProps and useOmitProps to accept multiple extraProps and streamline prop merging with a new mergeExtraProps function

--- src/composables/use-props.ts
+++ src/composables/use-props.ts
@@ -1,10 +1,24 @@
-import { computed } from 'vue';
+import { computed, toValue } from 'vue';
+import type { MaybeRefOrGetter } from 'vue';
 import { omit, pick } from '../shared';
 
-export function usePickProps<T extends object, K extends keyof T>(props: T, keys: K[], extraProps: object = {}) {
-  return computed(() => ({ ...pick(props, keys), ...extraProps }));
+export function usePickProps<T extends object, K extends keyof T>(
+  props: T,
+  keys: K[],
+  ...extraProps: MaybeRefOrGetter<object>[]
+) {
+  return computed(() => ({ ...pick(props, keys), ...mergeExtraProps(...extraProps) }));
 }
 
-export function useOmitProps<T extends object, K extends keyof T>(props: T, keys: K[], extraProps: object = {}) {
-  return computed(() => ({ ...omit(props, keys), ...extraProps }));
+export function useOmitProps<T extends object, K extends keyof T>(
+  props: T,
+  keys: K[],
+  ...extraProps: MaybeRefOrGetter<object>[]
+) {
+  return computed(() => ({ ...omit(props, keys), ...mergeExtraProps(...extraProps) }));
+}
+
+function mergeExtraProps(...extraProps: MaybeRefOrGetter<object>[]) {
+  if (extraProps.length === 0) return {};
+  return extraProps.map(prop => toValue(prop) || {}).reduce((acc, curr) => ({ ...acc, ...curr }), {});
 }
