d573decdf2e234c0bd649b12ad960c5064c47559
Soybean <soybeanjs@outlook.com>
2025-06-08 16:05:33 +0800

    refactor(card): remove size examples from Card component to streamline template and enhance clarity

--- playground/src/examples/card/index.vue
+++ playground/src/examples/card/index.vue
@@ -1,33 +1,10 @@
 <script setup lang="ts">
 import { Rocket, X } from 'lucide-vue-next';
-import type { ThemeSize } from '@theme';
 import { Card } from '@ui';
-
-const sizes: ThemeSize[] = ['xs', 'sm', 'md', 'lg', 'xl', '2xl'];
 </script>
 
 <template>
   <Card title="Card" :ui="{ content: 'flex-c-stretch gap-4' }">
-    <Card title="Size" split>
-      <div class="w-1/2 i-flex-c pr-10px lt-sm:(w-full pr-0)">
-        <Card v-for="size in sizes" :key="size" :title="`Size: ${size}`" :size="size" class="mb-12px">
-          <template #extra>
-            <span>extra</span>
-          </template>
-          <p class="text-gray-500 dark:text-neutral-400">Content</p>
-          <template #footer>Footer</template>
-        </Card>
-      </div>
-      <div class="w-1/2 i-flex-c pl-10px lt-sm:(w-full pl-0)">
-        <Card v-for="size in sizes" :key="size" :title="`Size: ${size}`" :size="size" split class="mb-12px">
-          <template #extra>
-            <X />
-          </template>
-          <p class="text-gray-500 dark:text-neutral-400">Content</p>
-          <template #footer>Footer</template>
-        </Card>
-      </div>
-    </Card>
     <Card title="More" split :ui="{ content: 'flex-c-stretch gap-4' }">
       <Card>
         <p class="text-gray-500 dark:text-neutral-400">Only Content</p>
