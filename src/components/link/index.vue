5812f458671c36615c567210eb1dbc6ab4046248
Soybean <soybeanjs@outlook.com>
2025-06-13 00:48:28 +0800

    refactor(link): consolidate Nuxt and Router link components into a single Link component with dynamic resolution

--- src/components/link/index.vue
+++ src/components/link/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { computed, defineAsyncComponent, useAttrs } from 'vue';
-import { isNuxt } from '@headless/shared';
+import { computed, resolveComponent, useAttrs } from 'vue';
+import { useConfigProvider } from '../config-provider/context';
 import { Primitive } from '../primitive';
 import type { LinkProps } from './types';
 
@@ -22,6 +22,15 @@ const props = withDefaults(defineProps<LinkProps>(), {
 
 const attrs = useAttrs();
 
+const { nuxt } = useConfigProvider('Link');
+
+const LinkComponent = computed(() => {
+  if (nuxt.value) {
+    return resolveComponent('NuxtLink');
+  }
+  return resolveComponent('RouterLink');
+});
+
 const isExternal = computed(() => {
   if (props.external) {
     return true;
@@ -42,23 +51,28 @@ const forwarded = computed(() => ({
   ...attrs,
   ...props,
   to: isExternal.value ? undefined : props.to,
-  href: isExternal.value ? props.to || props.href : props.href
+  href: isExternal.value ? props.to || props.href : props.href,
+  ariaDisabled: props.disabled ? 'true' : undefined,
+  role: props.disabled ? 'link' : undefined,
+  tabindex: props.disabled ? -1 : undefined
 }));
-
-const BaseLink = defineAsyncComponent(() => (isNuxt ? import('./nuxt-link.vue') : import('./router-link.vue')));
 </script>
 
 <template>
-  <Primitive
-    v-if="isExternal"
-    v-bind="forwarded"
-    :aria-disabled="disabled ? 'true' : undefined"
-    :role="disabled ? 'link' : undefined"
-    :tabindex="disabled ? -1 : undefined"
-  >
+  <Primitive v-if="isExternal" v-bind="forwarded">
     <slot />
   </Primitive>
-  <BaseLink v-else v-bind="forwarded">
-    <slot />
-  </BaseLink>
+  <component :is="LinkComponent" v-else v-slot="slotProps" v-bind="forwarded">
+    <template v-if="custom">
+      <Primitive
+        v-bind="forwarded"
+        :class="slotProps?.isActive ? activeClass : inactiveClass"
+        :href="slotProps?.href"
+        @click="slotProps?.navigate"
+      >
+        <slot />
+      </Primitive>
+    </template>
+    <slot v-else />
+  </component>
 </template>
