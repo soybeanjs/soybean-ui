79c18e478f97b8afb87a604f9fc0e66d4f4505e8
Soybean <soybeanjs@outlook.com>
2025-06-14 03:23:52 +0800

    refactor(button-group): add RTL support to horizontal orientation variant for improved layout consistency

--- ui/variants/button-group.ts
+++ ui/variants/button-group.ts
@@ -5,7 +5,7 @@ export const buttonGroupVariants = tv({
   base: `[&>*]:relative focus-visible:[&>*]:z-2 not-first:not-last:[&>*]:rd-0`,
   variants: {
     orientation: {
-      horizontal: `inline-flex not-last:[&>*]:border-r-0 first:[&>*]:rd-r-0 last:[&>*]:rd-l-0`,
+      horizontal: `inline-flex not-last:[&>*]:border-r-0 first:[&>*]:rd-r-0 last:[&>*]:rd-l-0 rtl:flex-row-reverse`,
       vertical: `flex flex-col not-last:[&>*]:border-b-0 first:[&>*]:rd-b-0 last:[&>*]:rd-t-0`
     }
   },
