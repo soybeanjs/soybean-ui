d8a3b1325cf47a8068500d0dc23832363d6f9ea6
Soybean <soybeanjs@outlook.com>
2025-06-14 13:15:18 +0800

    refactor(roving-focus): simplify ButtonGroup examples by removing redundant cards and enhancing layout with looping options

--- playground/src/examples/roving-focus/index.vue
+++ playground/src/examples/roving-focus/index.vue
@@ -16,7 +16,7 @@ function setDir() {
 </script>
 
 <template>
-  <SCard title="Roving Focus" split :dir="dir" :ui="{ content: 'flex-c gap-3' }">
+  <SCard title="Roving Focus" split>
     <template #extra>
       <div class="flex items-center gap-3">
         <span>
@@ -29,60 +29,61 @@ function setDir() {
         </SButton>
       </div>
     </template>
+    <div :dir="dir" class="flex flex-col gap-3">
+      <SCard title="no orientation (both) + no looping" split>
+        <ButtonGroup :dir="dir" default-value="two">
+          <Button value="one">One</Button>
+          <Button value="two">Two</Button>
+          <Button disabled value="three">Three</Button>
+          <Button value="four">Four</Button>
+        </ButtonGroup>
+      </SCard>
 
-    <SCard title="no orientation (both) + no looping" split>
-      <ButtonGroup :dir="dir" default-value="two">
-        <Button value="one">One</Button>
-        <Button value="two">Two</Button>
-        <Button disabled value="three">Three</Button>
-        <Button value="four">Four</Button>
-      </ButtonGroup>
-    </SCard>
+      <SCard title="no orientation (both) + looping" split>
+        <ButtonGroup :dir="dir" loop>
+          <Button value="one">One</Button>
+          <Button value="hidden" :style="{ display: 'none' }">Hidden</Button>
+          <Button value="two">Two</Button>
+          <Button disabled value="three">Three</Button>
+          <Button value="four">Four</Button>
+        </ButtonGroup>
+      </SCard>
 
-    <SCard title="no orientation (both) + looping" split>
-      <ButtonGroup :dir="dir" loop>
-        <Button value="one">One</Button>
-        <Button value="hidden" :style="{ display: 'none' }">Hidden</Button>
-        <Button value="two">Two</Button>
-        <Button disabled value="three">Three</Button>
-        <Button value="four">Four</Button>
-      </ButtonGroup>
-    </SCard>
+      <SCard title="horizontal orientation + no looping" split>
+        <ButtonGroup orientation="horizontal" :dir="dir">
+          <Button value="one">One</Button>
+          <Button value="two">Two</Button>
+          <Button disabled value="three">Three</Button>
+          <Button value="four">Four</Button>
+        </ButtonGroup>
+      </SCard>
 
-    <SCard title="horizontal orientation + no looping" split>
-      <ButtonGroup orientation="horizontal" :dir="dir">
-        <Button value="one">One</Button>
-        <Button value="two">Two</Button>
-        <Button disabled value="three">Three</Button>
-        <Button value="four">Four</Button>
-      </ButtonGroup>
-    </SCard>
+      <SCard title="horizontal orientation + looping" split>
+        <ButtonGroup orientation="horizontal" :dir="dir" loop>
+          <Button value="one">One</Button>
+          <Button value="two">Two</Button>
+          <Button disabled value="three">Three</Button>
+          <Button value="four">Four</Button>
+        </ButtonGroup>
+      </SCard>
 
-    <SCard title="horizontal orientation + looping" split>
-      <ButtonGroup orientation="horizontal" :dir="dir" loop>
-        <Button value="one">One</Button>
-        <Button value="two">Two</Button>
-        <Button disabled value="three">Three</Button>
-        <Button value="four">Four</Button>
-      </ButtonGroup>
-    </SCard>
+      <SCard title="vertical orientation + no looping" split>
+        <ButtonGroup orientation="vertical" :dir="dir" class="w-fit">
+          <Button value="one">One</Button>
+          <Button value="two">Two</Button>
+          <Button disabled value="three">Three</Button>
+          <Button value="four">Four</Button>
+        </ButtonGroup>
+      </SCard>
 
-    <SCard title="vertical orientation + no looping" split>
-      <ButtonGroup orientation="vertical" :dir="dir" class="w-fit">
-        <Button value="one">One</Button>
-        <Button value="two">Two</Button>
-        <Button disabled value="three">Three</Button>
-        <Button value="four">Four</Button>
-      </ButtonGroup>
-    </SCard>
-
-    <SCard title="vertical orientation + looping" split>
-      <ButtonGroup orientation="vertical" :dir="dir" loop class="w-fit">
-        <Button value="one">One</Button>
-        <Button value="two">Two</Button>
-        <Button disabled value="three">Three</Button>
-        <Button value="four">Four</Button>
-      </ButtonGroup>
-    </SCard>
+      <SCard title="vertical orientation + looping" split>
+        <ButtonGroup orientation="vertical" :dir="dir" loop class="w-fit">
+          <Button value="one">One</Button>
+          <Button value="two">Two</Button>
+          <Button disabled value="three">Three</Button>
+          <Button value="four">Four</Button>
+        </ButtonGroup>
+      </SCard>
+    </div>
   </SCard>
 </template>
