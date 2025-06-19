26ecace498fe23a0f82bb5ff4de12c1e7d86b2a9
Soybean <soybeanjs@outlook.com>
2025-06-20 02:04:42 +0800

    refactor(select): remove unused valueComparator and compare functions to streamline code

--- src/components/select/shared.ts
+++ src/components/select/shared.ts
@@ -1,4 +1,3 @@
-import { isEqual } from '../../shared';
 import type { AcceptableValue } from '../../types';
 
 export const selectCssVars = {
@@ -14,35 +13,6 @@ export const SELECTION_KEYS = [' ', 'Enter'];
 export const CONTENT_MARGIN = 10;
 
 export const SELECT_EVENT = 'select.select';
-
-export function valueComparator<T>(
-  value: T | T[] | undefined,
-  currentValue: T,
-  comparator?: string | ((a: T, b: T) => boolean)
-) {
-  if (value === undefined) return false;
-  else if (Array.isArray(value)) {
-    return value.some(val => compare(val, currentValue, comparator));
-  }
-
-  return compare(value, currentValue, comparator);
-}
-
-export function compare<T>(value?: T, currentValue?: T, comparator?: string | ((a: T, b: T) => boolean)) {
-  if (value === undefined || currentValue === undefined) return false;
-
-  if (typeof value === 'string') {
-    return value === currentValue;
-  }
-
-  if (typeof comparator === 'function') {
-    return comparator(value, currentValue);
-  }
-
-  if (typeof comparator === 'string') return value?.[comparator as keyof T] === currentValue?.[comparator as keyof T];
-
-  return isEqual(value, currentValue);
-}
 
 export function shouldShowPlaceholder(value?: AcceptableValue | AcceptableValue[]): boolean {
   return value === undefined || value === null || value === '' || (Array.isArray(value) && value.length === 0);
