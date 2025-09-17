87dcc838674c00cf009785ca429f6bb270974312
Soybean <soybeanjs@outlook.com>
2025-09-17 17:59:14 +0800

    feat(shared): add findValuesBetween function to extract subarray between two values

--- src/shared/object.ts
+++ src/shared/object.ts
@@ -1,3 +1,5 @@
+import { isEqual } from './comparison';
+
 /**
  * Select specified properties from an object
  *
@@ -134,4 +136,28 @@ export function getNextMatch(values?: string[], search?: string, currentMatch?: 
   }
 
   return undefined;
+}
+
+/**
+ * The function `findValuesBetween` takes an array and two values, then returns a subarray containing elements between
+ * the first occurrence of the start value and the first occurrence of the end value in the array.
+ *
+ * @param {T[]} array - The `array` parameter is an array of values of type `T`.
+ * @param {T} start - The `start` parameter is the value that marks the beginning of the range you want to find in the
+ *   array.
+ * @param {T} end - The `end` parameter in the `findValuesBetween` function represents the end value that you want to
+ *   find in the array. This function will return a subarray of values that are between the `start` and `end` values in
+ *   the original array.
+ * @returns The `findValuesBetween` function returns an array of values from the input array that are between the
+ *   `start` and `end` values (inclusive). If either the `start` or `end` values are not found in the input array, an
+ *   empty array is returned.
+ */
+export function findValuesBetween<T>(array: T[], start: T, end: T) {
+  const startIndex = array.findIndex(i => isEqual(i, start));
+  const endIndex = array.findIndex(i => isEqual(i, end));
+  if (startIndex === -1 || endIndex === -1) return [];
+
+  const [minIndex, maxIndex] = ([startIndex, endIndex] as [number, number]).sort((a, b) => a - b);
+
+  return array.slice(minIndex, maxIndex + 1);
 }
