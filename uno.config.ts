4f600cc366118e43977aed4ffa8a2f444785c473
Soybean <soybeanjs@outlook.com>
2025-10-19 21:19:13 +0800

    chore(deps): update deps

--- uno.config.ts
+++ uno.config.ts
@@ -15,7 +15,6 @@ export default defineConfig<Theme>({
     presetWind3({ dark: 'class' }),
     presetTypography(),
     presetAnimations(),
-    // @ts-expect-error ignore type
     presetSoybeanJS(),
     // @ts-expect-error ignore type
     presetSoybeanUI()
