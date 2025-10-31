54ec9ddd92a2789e4071b8c480958aba78b10c60
Soybean <soybeanjs@outlook.com>
2025-11-01 04:01:09 +0800

    optimize(examples): replace Arrow and AspectRatio components with SArrow and SAspectRatio for consistency in UI component usage

--- playground/src/examples/arrow/index.vue
+++ playground/src/examples/arrow/index.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { SCard } from '@ui';
-import { Arrow } from '@headless';
+import { SArrow, SCard } from '@ui';
 </script>
 
 <template>
-  <SCard title="Arrow" :ui="{ content: 'flex-center gap-4 h-20 bg-gray-100 dark:bg-gray-900' }">
-    <Arrow class="text-2xl fill-popover stroke-border" />
-    <Arrow class="text-2xl fill-popover-foreground" />
+  <SCard title="Arrow" :ui="{ content: 'flex gap-4' }">
+    <SArrow class="text-2xl fill-popover stroke-border" />
+    <SArrow class="text-2xl fill-popover-foreground" />
   </SCard>
 </template>
--- playground/src/examples/aspect-ratio/index.vue
+++ playground/src/examples/aspect-ratio/index.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import { SCard } from '@ui';
-import { AspectRatio } from '@headless';
+import { SAspectRatio, SCard } from '@ui';
 </script>
 
 <template>
   <SCard title="Aspect Ratio">
     <div class="w-100 lt-sm:w-full">
-      <AspectRatio :ratio="16 / 9" class="bg-muted">
+      <SAspectRatio :ratio="16 / 9" class="bg-muted">
         <img
           src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
           class="h-full w-full rounded-md object-cover"
         />
-      </AspectRatio>
+      </SAspectRatio>
     </div>
   </SCard>
 </template>
