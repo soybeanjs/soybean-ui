a9fa32b829cf646fb02baa509b988e3704d9fe37
Azir-11 <2075125282@qq.com>
2025-11-24 19:32:17 +0800

    feat(packages): modify the width of the page center within the document.

--- docs/src/layouts/default.vue
+++ docs/src/layouts/default.vue
@@ -3,9 +3,15 @@
 <template>
   <div class="[--app-header:3.75rem] h-full pt-[calc(var(--app-header)+1rem)] text-sm">
     <AppHeader />
-    <div class="fixed top-[--app-header] left-0 w-50 h-[calc(100vh-var(--app-header))]">
-      <SiderMenu />
+    <div class="max-w-[1440px] mx-auto flex h-full px-6">
+      <aside
+        class="sticky top-[calc(var(--app-header)+1rem)] w-50 h-[calc(100vh-var(--app-header)-2rem)] overflow-y-auto hidden md:block shrink-0"
+      >
+        <SiderMenu />
+      </aside>
+      <main class="flex-1 min-w-0 pl-8">
+        <RouterView />
+      </main>
     </div>
-    <RouterView />
   </div>
 </template>
