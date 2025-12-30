9fc807ada1b4585e6fc46fdb3799f2f17924cb26
Soybean <soybeanjs@outlook.com>
2025-12-30 17:40:32 +0800

    refactor(arrow): simplify Arrow component by removing unused context and props, and directly using SVG markup

--- headless/src/components/arrow/context.ts
+++
@@ -1,8 +0,0 @@
-import type { ComputedRef } from 'vue';
-import { useContext } from '../../composables';
-import type { ClassValue } from '../../types';
-
-export const [provideArrowThemeContext, useArrowThemeContext] = useContext(
-  'ArrowTheme',
-  (cls: ComputedRef<ClassValue>) => cls
-);
--- headless/src/components/arrow/types.ts
+++ headless/src/components/arrow/types.ts
@@ -1,17 +1,3 @@
 import type { SVGAttributes } from 'vue';
-import type { PrimitiveProps } from '../primitive/types';
 
-export interface ArrowProps extends PrimitiveProps, /** @vue-ignore */ SVGAttributes {
-  /**
-   * The width of the arrow in pixels.
-   *
-   * @defaultValue 1em
-   */
-  width?: number | string;
-  /**
-   * The height of the arrow in pixels.
-   *
-   * @defaultValue 0.5em
-   */
-  height?: number | string;
-}
+export interface ArrowProps extends /** @vue-ignore */ SVGAttributes {}
