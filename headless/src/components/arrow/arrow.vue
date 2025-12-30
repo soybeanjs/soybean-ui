49949eb2c8c89ea881ca22543f77ee97834473a1
Soybean <soybeanjs@outlook.com>
2025-12-30 17:40:32 +0800

    refactor(arrow): simplify Arrow component by removing unused context and props, and directly using SVG markup

--- headless/src/components/arrow/arrow.vue
+++ headless/src/components/arrow/arrow.vue
@@ -1,38 +1,15 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { Primitive } from '../primitive';
-import { useArrowThemeContext } from './context';
 import type { ArrowProps } from './types';
 
 defineOptions({
   name: 'Arrow'
 });
 
-const props = withDefaults(defineProps<ArrowProps>(), {
-  as: 'svg',
-  width: '1em',
-  height: '0.5em'
-});
-
-const themeContext = useArrowThemeContext();
-
-const cls = computed(() => themeContext?.value);
-
-const isTemplate = computed(() => props.as === 'template');
+defineProps<ArrowProps>();
 </script>
 
 <template>
-  <Primitive
-    :as="as"
-    :as-child="asChild"
-    :width="isTemplate ? undefined : width"
-    :height="isTemplate ? undefined : height"
-    :class="cls"
-    :viewBox="isTemplate ? undefined : '0 0 12 6'"
-    :preserveAspectRatio="isTemplate ? undefined : 'none'"
-  >
-    <slot>
-      <path d="M0 0L6 6L12 0" />
-    </slot>
-  </Primitive>
+  <svg viewBox="0 0 12 6" preserveAspectRatio="none">
+    <path d="M 0,0 L 6,6 L 12,0" />
+  </svg>
 </template>
--- headless/src/components/arrow/index.ts
+++ headless/src/components/arrow/index.ts
@@ -1,5 +1,3 @@
 export { default as Arrow } from './arrow.vue';
 
-export { provideArrowThemeContext } from './context';
-
 export type { ArrowProps } from './types';
