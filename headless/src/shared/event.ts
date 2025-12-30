3401f0384bb73ae4e50b84d4338c10ccf2a821c5
Soybean <soybeanjs@outlook.com>
2025-12-30 17:43:26 +0800

    fix(event): add null check for event target in handleAndDispatchCustomEvent function

--- headless/src/shared/event.ts
+++ headless/src/shared/event.ts
@@ -4,6 +4,8 @@ export function handleAndDispatchCustomEvent<E extends CustomEvent, OriginalEven
   detail: { originalEvent: OriginalEvent } & (E extends CustomEvent<infer D> ? D : never)
 ) {
   const target = detail.originalEvent.target;
+  if (!target) return;
+
   const event = new CustomEvent(name, {
     bubbles: false,
     cancelable: true,
