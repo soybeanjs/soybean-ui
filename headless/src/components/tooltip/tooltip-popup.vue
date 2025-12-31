4d8da1004192e93fd4d5a368e7c4dc0ab4729454
Soybean <soybeanjs@outlook.com>
2025-12-31 21:51:36 +0800

    optimize(tooltip): remove TooltipPositioner context and update related components for improved clarity and functionality

--- headless/src/components/tooltip/tooltip-popup.vue
+++ headless/src/components/tooltip/tooltip-popup.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { computed, useSlots } from 'vue';
-import type { CSSProperties } from 'vue';
 import { useForwardElement } from '../../composables';
 import { getAriaLabelByVNodeList } from '../../shared';
 import { PopperPopup } from '../popper';
 import { popperCssVars } from '../popper/shared';
 import { VisuallyHidden } from '../visually-hidden';
-import { useTooltipPositionerContext, useTooltipRootContext } from './context';
+import { useTooltipRootContext } from './context';
 import { tooltipCssVars } from './shared';
 import type { TooltipPopupProps } from './types';
 
@@ -20,7 +19,6 @@ const slots = useSlots();
 const defaultSlot = computed(() => slots.default?.());
 
 const { popupId, dataState, initPopupId, onPopupElementChange } = useTooltipRootContext('TooltipPopup');
-const { pointerEvents } = useTooltipPositionerContext('TooltipPopup');
 const [, setPopupElement] = useForwardElement(onPopupElementChange);
 
 const cssVarsStyle = {
@@ -30,11 +28,6 @@ const cssVarsStyle = {
   [tooltipCssVars.anchorWidth]: `var(${popperCssVars.anchorWidth})`,
   [tooltipCssVars.anchorHeight]: `var(${popperCssVars.anchorHeight})`
 };
-
-const style = computed<CSSProperties>(() => ({
-  ...cssVarsStyle,
-  pointerEvents: pointerEvents.value
-}));
 
 const ariaLabel = computed(() => {
   if (props.ariaLabel) {
@@ -48,7 +41,7 @@ initPopupId();
 </script>
 
 <template>
-  <PopperPopup :ref="setPopupElement" data-dismissable-layer :data-state="dataState" :style="style">
+  <PopperPopup :ref="setPopupElement" data-dismissable-layer :data-state="dataState" :style="cssVarsStyle">
     <slot />
     <VisuallyHidden :id="popupId" role="tooltip">
       {{ ariaLabel }}
