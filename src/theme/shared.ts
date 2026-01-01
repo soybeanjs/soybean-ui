81dff657351af41357371f4c2f9ccb210e83479a
Soybean <soybeanjs@outlook.com>
2026-01-02 06:48:18 +0800

    feat(theme): enhance mergeSlotVariants function to include root class merging

--- src/theme/shared.ts
+++ src/theme/shared.ts
@@ -1,14 +1,25 @@
 import type { ClassValue } from '@soybeanjs/headless';
 import { cn } from './merge';
 
+/**
+ * Merge slot variants with props and root class.
+ * @param variants - The variants to merge.
+ * @param props - The props to merge.
+ * @param rootCls - The root class to merge.
+ * @returns The merged variants.
+ */
 export function mergeSlotVariants<T extends Record<string, (...args: any[]) => string>, P extends Record<keyof T, any>>(
   variants: T,
-  props?: Partial<P>
+  props?: Partial<P>,
+  rootCls?: ClassValue | unknown
 ) {
   return Object.entries(variants).reduce(
     (acc, [$key, variant]) => {
       const key = $key as keyof T;
-      acc[key] = cn(variant(), props?.[key]);
+
+      const rootClass = key === 'root' ? rootCls : undefined;
+
+      acc[key] = cn(variant(), props?.[key], rootClass as ClassValue);
       return acc;
     },
     {} as Record<keyof T, string>
