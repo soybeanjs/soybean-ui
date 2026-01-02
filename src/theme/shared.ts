8f079dd14527f2e2472c0465178da6fc7cce38e5
Soybean <soybeanjs@outlook.com>
2026-01-03 04:22:58 +0800

    refactor(theme): update mergeSlotVariants and mergeUi functions to support multiple UI sources for improved class merging

--- src/theme/shared.ts
+++ src/theme/shared.ts
@@ -4,31 +4,49 @@ import { cn } from './merge';
 /**
  * Merge slot variants with props and root class.
  * @param variants - The variants to merge.
- * @param props - The props to merge.
- * @param rootCls - The root class to merge.
+ * @param uis - The uis to merge.
  * @returns The merged variants.
  */
-export function mergeSlotVariants<T extends Record<string, (...args: any[]) => string>, P extends Record<keyof T, any>>(
-  variants: T,
-  props?: Partial<P>,
-  rootCls?: ClassValue | unknown
-) {
+export function mergeSlotVariants<
+  T extends Record<string, (...args: any[]) => string>,
+  P extends Record<keyof T, ClassValue>
+>(variants: T, ...uis: Array<Partial<P> | null | undefined>) {
   return Object.entries(variants).reduce(
     (acc, [$key, variant]) => {
       const key = $key as keyof T;
 
-      const rootClass = key === 'root' ? rootCls : undefined;
-
-      acc[key] = cn(variant(), props?.[key], rootClass as ClassValue);
+      acc[key] = cn(
+        variant(),
+        ...uis
+          .filter(Boolean)
+          .map(u => u?.[key])
+          .filter(Boolean)
+      );
       return acc;
     },
     {} as Record<keyof T, string>
   );
 }
 
-export function mergeUi<S extends Record<string, ClassValue>, T extends Partial<S>>(target: T, source: S) {
-  return Object.entries(target).reduce((acc, [key, value]) => {
-    Object.assign(acc, { [key]: cn(value, source[key]) });
+export function mergeUi<T extends Record<string, ClassValue>, S extends Record<keyof T, ClassValue>>(
+  target?: T,
+  ...sources: Array<Partial<S> | null | undefined>
+) {
+  if (!target) return {} as T;
+
+  return Object.entries(target).reduce((acc, [$key, value]) => {
+    const key = $key as keyof T;
+
+    Object.assign(acc, {
+      [key]: cn(
+        value,
+        ...sources
+          .filter(Boolean)
+          .map(s => s?.[key])
+          .filter(Boolean)
+      )
+    });
+
     return acc;
-  }, {} as S);
+  }, {} as T);
 }
