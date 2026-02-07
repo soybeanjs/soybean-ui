1dadda7b066970648f18caddeb5db7f0269ae105
Soybean <soybeanjs@outlook.com>
2026-02-08 01:36:25 +0800

    fix(page-tabs): fix scroll centerX value

--- headless/src/components/page-tabs/hooks.ts
+++ headless/src/components/page-tabs/hooks.ts
@@ -1,11 +1,9 @@
-import { computed, watchEffect, nextTick } from 'vue';
+import { watchEffect, nextTick } from 'vue';
 import type { ShallowRef } from 'vue';
 import { useForwardElement } from '../../composables';
 
 export function usePageTabsScroll(activeValue: ShallowRef<string>) {
   const [rootElement, setRootElement] = useForwardElement();
-
-  const centerX = computed(() => (rootElement.value?.offsetWidth ?? 0) / 2);
 
   const onWheel = (event: WheelEvent) => {
     if (rootElement.value) {
@@ -14,14 +12,16 @@ export function usePageTabsScroll(activeValue: ShallowRef<string>) {
   };
 
   watchEffect(async () => {
-    if (!rootElement.value || !centerX.value || !activeValue.value) return;
+    if (!rootElement.value || !activeValue.value) return;
 
     await nextTick();
 
     const activeElement = rootElement.value.querySelector<HTMLElement>(`[data-value="${activeValue.value}"]`);
     if (!activeElement) return;
 
-    const offsetX = activeElement.offsetLeft + activeElement.offsetWidth / 2 - centerX.value;
+    const centerX = (rootElement.value?.offsetWidth ?? 0) / 2;
+
+    const offsetX = activeElement.offsetLeft + activeElement.offsetWidth / 2 - centerX;
 
     rootElement.value.scrollTo({ left: offsetX, behavior: 'smooth' });
   });
