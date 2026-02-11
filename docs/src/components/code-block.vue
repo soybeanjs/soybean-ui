643dbce19f67c66203503bdbbfbbc16daa7b9389
Soybean <soybeanjs@outlook.com>
2026-02-11 11:29:14 +0800

    fix(docs): fix code-block style

--- docs/src/components/code-block.vue
+++ docs/src/components/code-block.vue
@@ -14,6 +14,7 @@ const wrapper = useTemplateRef('wrapper');
 async function renderCode() {
   const html = await codeToHtml(props.code, {
     lang: props.lang,
+    defaultColor: false,
     themes: {
       light: 'one-light',
       dark: 'one-dark-pro'
@@ -23,6 +24,7 @@ async function renderCode() {
   if (wrapper.value) {
     wrapper.value.innerHTML = html;
   }
+  console.log('html: ', html);
 }
 
 onMounted(() => {
