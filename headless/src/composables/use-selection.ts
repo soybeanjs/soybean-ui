3ded4f750a1a59c6cb4341c31e6cc9b04b681c84
Soybean <soybeanjs@outlook.com>
2026-02-28 11:41:40 +0800

    optimize(composables): update useSelection to support MaybeRefOrGetter for props

--- headless/src/composables/use-selection.ts
+++ headless/src/composables/use-selection.ts
@@ -1,17 +1,20 @@
-import { computed } from 'vue';
+import { computed, toValue } from 'vue';
+import type { MaybeRefOrGetter } from 'vue';
 import { isNullish } from '../shared';
 import type { DefinedValue, SelectionProps } from '../types';
 import { useControllableState } from './use-controllable-state';
 
 export function useSelection<M extends boolean = false, N extends DefinedValue = string>(
-  props: SelectionProps<M, N>,
+  props: MaybeRefOrGetter<SelectionProps<M, N>>,
   onUpdateModelValue: (value: NonNullable<SelectionProps<M, N>['modelValue']>) => void
 ) {
+  const propsRef = computed(() => toValue(props));
+
   // @ts-expect-error ignore type: Vue props Boolean Casting [https://vuejs.org/guide/components/props.html#boolean-casting]
-  const isMultiple = computed(() => Boolean(props.multiple) || props.multiple === '');
+  const isMultiple = computed(() => Boolean(propsRef.value.multiple) || propsRef.value.multiple === '');
 
   const modelValue = useControllableState(
-    () => props.modelValue,
+    () => propsRef.value.modelValue,
     value => {
       onUpdateModelValue(value!);
     },
@@ -19,12 +22,19 @@ export function useSelection<M extends boolean = false, N extends DefinedValue =
   );
 
   function getSelectionDefaultValue() {
-    if (!isNullish(props.defaultValue)) {
-      return props.defaultValue;
+    if (!isNullish(propsRef.value.defaultValue)) {
+      return propsRef.value.defaultValue;
     }
 
     return (isMultiple.value ? [] : undefined) as SelectionProps<M, N>['modelValue'];
   }
+
+  const isValueSelected = (value: N) => {
+    if (isMultiple.value && Array.isArray(modelValue.value)) {
+      return modelValue.value.includes(value);
+    }
+    return modelValue.value === value;
+  };
 
   const isEmptyModelValue = computed(() => {
     if (isMultiple.value && Array.isArray(modelValue.value)) {
@@ -57,9 +67,27 @@ export function useSelection<M extends boolean = false, N extends DefinedValue =
     modelValue.value = values as SelectionProps<M, N>['modelValue'];
   };
 
+  const setModelValue = (value: N[] | N | undefined) => {
+    if (isMultiple.value && Array.isArray(value)) {
+      modelValue.value = value as N[] as SelectionProps<M, N>['modelValue'];
+      return;
+    }
+
+    if (!isMultiple.value && !Array.isArray(value)) {
+      modelValue.value = value as N as SelectionProps<M, N>['modelValue'];
+    }
+  };
+
+  const resetModelValue = () => {
+    modelValue.value = (isMultiple.value ? ([] as N[]) : undefined) as SelectionProps<M, N>['modelValue'];
+  };
+
   return {
     modelValue,
     onModelValueChange,
+    setModelValue,
+    resetModelValue,
+    isValueSelected,
     isMultiple,
     isEmptyModelValue
   };
