30fd60fac50447ce88c511482f864467bbc2f3f2
Soybean <soybeanjs@outlook.com>
2026-03-06 01:42:48 +0800

    fix(dialog): remove unnecessary sm: prefix from rounded-lg class

--- src/components/dialog/variants.ts
+++ src/components/dialog/variants.ts
@@ -9,7 +9,7 @@ export const dialogVariants = tv({
       `data-[state=closed]:animate-out data-[state=closed]:fade-out-0`
     ],
     content: [
-      `fixed left-[50%] top-[50%] z-50 flex flex-col w-full border bg-background shadow-lg outline-none translate-x-[-50%] translate-y-[-50%] md:w-full duration-200 sm:rounded-lg`,
+      `fixed left-[50%] top-[50%] z-50 flex flex-col w-full border bg-background shadow-lg outline-none translate-x-[-50%] translate-y-[-50%] md:w-full duration-200 rounded-lg`,
       `data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-left-1/2`,
       `data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2`
     ],
