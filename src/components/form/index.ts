c0ee8467a25034f0904fa7f9038b91f19a8c498c
Soybean <soybeanjs@outlook.com>
2026-03-05 23:08:53 +0800

    feat(form): add SFormFieldBase component export

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,4 +1,5 @@
 export { default as SForm } from './form.vue';
+export { default as SFormFieldBase } from './form-field-base.vue';
 export { useForm } from './use-form';
 
 export type { FormLabelProps, FormDescriptionProps, FormErrorProps } from '@soybeanjs/headless/form';
