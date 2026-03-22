25840a45663b601b5783527135042ef0565b076b
Soybean <soybeanjs@outlook.com>
2026-03-22 16:05:36 +0800

    fix(table): reorder exports for consistency and clarity

--- src/components/table/index.ts
+++ src/components/table/index.ts
@@ -1,5 +1,6 @@
-export { default as STable } from './table.vue';
 export { TableRow as STableRow, TableCell as STableCell } from '@soybeanjs/headless';
+export type { TableColumnCheck } from '@soybeanjs/hooks';
+export { default as STable } from './table.vue';
 export { useTable, usePaginatedTable } from './hooks';
 
 export type * from './types';
