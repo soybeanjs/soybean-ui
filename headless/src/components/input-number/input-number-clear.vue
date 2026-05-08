07fa16d1923a5390b68af3e61e6b5f40311f7a8a
soybeanfe <honghuangdc@gmail.com>
2026-05-08 18:40:41 +0800

    fix(input-number): add ariaLabel for InputNumberClear

--- headless/src/components/input-number/input-number-clear.vue
+++ headless/src/components/input-number/input-number-clear.vue
@@ -27,7 +27,14 @@ const handleClear = (event: PointerEvent) => {
 </script>
 
 <template>
-  <Button v-bind="props" data-slot="clearable" :class="cls" :disabled="disabled" @click="handleClear">
+  <Button
+    v-bind="props"
+    data-slot="clearable"
+    :class="cls"
+    aria-label="Clear value"
+    :disabled="disabled"
+    @click="handleClear"
+  >
     <slot>
       <Icon icon="lucide:x" />
     </slot>
