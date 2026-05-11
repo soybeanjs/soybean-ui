4d7478d4454e9ea55b7336111502d021b8fc5934
honghuangdc <soybeanjs@outlook.com>
2026-05-11 12:37:28 +0800

    optimize(hover-card): simplify slot handling and remove unused variables

--- src/components/hover-card/hover-card.vue
+++ src/components/hover-card/hover-card.vue
@@ -2,7 +2,6 @@
 import { computed } from 'vue';
 import { HoverCardCompact, provideHoverCardUi } from '@soybeanjs/headless/hover-card';
 import { useForwardListeners, useOmitProps } from '@soybeanjs/headless/composables';
-import { keysOf } from '@soybeanjs/utils';
 import { mergeVariants } from '@/theme';
 import { hoverCardVariants } from './variants';
 import type { HoverCardProps, HoverCardEmits, HoverCardSlots } from './types';
@@ -18,13 +17,11 @@ const props = withDefaults(defineProps<HoverCardProps>(), {
 
 const emit = defineEmits<HoverCardEmits>();
 
-const slots = defineSlots<HoverCardSlots>();
+defineSlots<HoverCardSlots>();
 
 const forwardedProps = useOmitProps(props, ['class', 'size', 'ui']);
 
 const listeners = useForwardListeners(emit);
-
-const slotNames = computed(() => keysOf(slots));
 
 const ui = computed(() => {
   const variants = hoverCardVariants({
@@ -39,8 +36,9 @@ provideHoverCardUi(ui);
 
 <template>
   <HoverCardCompact v-bind="forwardedProps" v-on="listeners">
-    <template v-for="slotName in slotNames" :key="slotName" #[slotName]>
-      <slot :name="slotName" />
+    <template #trigger>
+      <slot name="trigger" />
     </template>
+    <slot />
   </HoverCardCompact>
 </template>
