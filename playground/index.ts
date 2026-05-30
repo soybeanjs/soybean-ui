ce7d7626fadf762ee9ab70859427f55532329e9c
soybeanfe <honghuangdc@gmail.com>
2026-05-30 14:47:59 +0800

    feat(playground): add internationalization setup and locale management

--- playground/index.ts
+++ playground/index.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import 'uno.css';
+import { setupI18n } from './i18n';
 import { router } from './router';
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(router);
+
+setupI18n(app);
 
 app.mount('#app');
