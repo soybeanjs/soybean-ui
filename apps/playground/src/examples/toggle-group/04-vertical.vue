e27e09a41829a099b4b86bbf796a40144aa2a729
Soybean <soybeanjs@outlook.com>
2026-08-11 22:32:26 +0800

    style(toggle-group): fix demo class
    调整垂直切换组示例的冗余样式，清理不必要的gap和flex布局类

--- apps/playground/src/examples/toggle-group/04-vertical.vue
+++ apps/playground/src/examples/toggle-group/04-vertical.vue
@@ -3,7 +3,7 @@ import { SToggleGroup, SToggleGroupItem } from '@soybeanjs/ui';
 </script>
 
 <template>
-  <SToggleGroup orientation="vertical" variant="outline" default-value="week" class="flex flex-wrap gap-3">
+  <SToggleGroup orientation="vertical" variant="outline" default-value="week">
     <SToggleGroupItem value="day">Day</SToggleGroupItem>
     <SToggleGroupItem value="week">Week</SToggleGroupItem>
     <SToggleGroupItem value="month">Month</SToggleGroupItem>
