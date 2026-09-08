27a41be8cd91a16a9030bbea977036f5de162ca1
soybeanfe <honghuangdc@gmail.com>
2026-09-08 19:13:39 +0800

    fix(locale): fix locale-toggler and unify locale code
    将zh_cn改为zh作为语言键名，替换原有的路由切换逻辑为直接调用setLocale方法

--- apps/docs/src/components/locale-toggler.vue
+++ apps/docs/src/components/locale-toggler.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import { useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
+import { setLocale } from 'ubean/client';
 import type { MenuOptionData } from '@soybeanjs/ui';
 import { snakeCase } from 'es-toolkit';
 
 const { t, locale } = useI18n();
-const router = useRouter();
-const switchLocalePath = useSwitchLocalePath();
 
 const iconMap: Record<string, string> = {
   en: 'lucide:spell-check-2',
@@ -26,11 +24,7 @@ const items = computed<MenuOptionData<string>[]>(() => {
 });
 
 const onSelectLocale = (item: MenuOptionData<string>) => {
-  const path = switchLocalePath(item.value);
-
-  if (path) {
-    router.push(path);
-  }
+  setLocale(item.value);
 };
 </script>
 
