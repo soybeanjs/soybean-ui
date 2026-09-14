1fc28e370b8fc2698e9c3af42e605a9a132d6e0c
honghuangdc <soybeanjs@outlook.com>
2026-09-14 15:12:04 +0800

    docs(i18n): add 'back to home' button text in English and Chinese locales

--- apps/docs/src/pages/[...all].vue
+++ apps/docs/src/pages/[...all].vue
@@ -9,6 +9,6 @@ const { t } = useI18n();
 <template>
   <div class="h-full w-full text-center">
     <h3 class="p-4 text-xl text-primary">{{ t('not_found_page') }}</h3>
-    <SButtonLink class="mt-4">Back to Home</SButtonLink>
+    <SButtonLink to="/" class="mt-4">{{ t('button.back_to_home') }}</SButtonLink>
   </div>
 </template>
